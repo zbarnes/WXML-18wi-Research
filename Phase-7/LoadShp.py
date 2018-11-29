280a47675e961197921a9d715dd5c21ad019e5c9
Zachary Barnes <33704301+zbarnes@users.noreply.github.com>
2018-11-28 19:51:30 -0800

    Update LoadShp.py
    Script now reads a .csv file and joins rows with .shp file.

--- Phase-7/LoadShp.py
+++ Phase-7/LoadShp.py
@@ -1,5 +1,21 @@
 from PyQt4 import QtGui
-#update file path for the shapefile in .zip
-zip_uri = '/vsizip/U:/GIS DATA/WA Shape/WA Shape.zip'
-shp =  QgsVectorLayer(zip_uri, 'WA Shape', 'ogr')
+#File paths must be updated
+#Load .shp file from a .zip file 
+shp_zip = '/vsizip/C:/Users/zbarnes/Downloads/WA Shape.zip/WA_final.shp'
+shp =  QgsVectorLayer(shp_zip, 'WA_final', 'ogr')
 QgsMapLayerRegistry.instance().addMapLayer(shp)
+
+#Load csv file
+csv_file = "file:///C:/Users/zbarnes/Downloads/TestMap.csv?delimiter=,"
+csv = QgsVectorLayer(csv_file, "TestMap", "delimitedtext")
+QgsMapLayerRegistry.instance().addMapLayer(csv)
+
+#Join csv file with header WA_GEO_ID and the WA_GEO_ID field in .shp
+shpJoinField='WA_GEO_ID'
+csvJoinField='WA_GEO_ID'
+joinObject = QgsVectorJoinInfo()
+joinObject.joinLayerId = csv.id()
+joinObject.joinFieldName = csvJoinField
+joinObject.targetFieldName = shpJoinField
+joinObject.memoryCache = True
+shp.addJoin(joinObject)
